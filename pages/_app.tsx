0b8d78b9ac28c2294104e2940825649603346ffa
Alfian Andi Nugraha <alfian.23andi@gmail.com>
2021-08-19 13:50:53 +0700

    feat(server-side styling): add material-ui with server-side styling

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,16 @@
 import type { AppProps } from 'next/app'
 import {ThemeProvider} from "@material-ui/styles";
 import overrideTheme from "@src/theme";
+import {useEffect} from "react";
 
 function MyApp({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    const jssStyles = document.querySelector('#jss-server-side');
+    if (jssStyles) {
+      jssStyles.parentElement?.removeChild(jssStyles);
+    }
+  }, []);
+
   return (
     <ThemeProvider theme={overrideTheme}>
       <Component {...pageProps} />
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,6 +1,29 @@
-import Document, {Head, Html, Main, NextScript} from "next/document";
+import Document, {DocumentContext, Head, Html, Main, NextScript} from "next/document";
+import {ServerStyleSheets} from "@material-ui/styles";
+import {AppType} from "next/dist/shared/lib/utils";
 
 class RootDocument extends Document {
+  static async getInitialProps(ctx: DocumentContext) {
+    const sheets = new ServerStyleSheets()
+    const originalRenderPage = ctx.renderPage
+
+    ctx.renderPage = () => {
+      return originalRenderPage({
+        enhanceApp: (App: AppType) => {
+          return (props) => {
+            return sheets.collect(<App {...props}/>)
+          }
+        }
+      })
+    }
+
+    const initialProps = await Document.getInitialProps(ctx)
+    return {
+      ...initialProps,
+      styles: [sheets.getStyleElement()]
+    }
+  }
+
   render() {
     return (
       <Html>
