a215708118c99899be27ea7ebd3986188884f4b1
Alfian Andi Nugraha <alfian.23andi@gmail.com>
2021-08-27 13:42:24 +0700

    feat(title): add title on _document

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -12,6 +12,7 @@ export default class MyDocument extends Document {
           <link rel="preconnect" href="https://fonts.gstatic.com"/>
           <link href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,400;0,700;1,400;1,700&display=swap"
                 rel="stylesheet"/>
+          <title>Dagallery</title>
         </Head>
         <body>
           <Main/>
